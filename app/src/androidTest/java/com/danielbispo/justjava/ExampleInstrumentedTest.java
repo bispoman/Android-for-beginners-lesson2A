177cfbc8745dfe322852f649ab04b9407605695f
Daniel Bispo <daniel.bispo@bemobi.com.br>
2016-12-12 19:01:42 -0200

    Primeiro commit com tudo que tenho, to em 'Quiz: Update Data'

---
+++ app/src/androidTest/java/com/danielbispo/justjava/ExampleInstrumentedTest.java
@@ -0,0 +1,26 @@
+package com.danielbispo.justjava;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Instrumentation test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() throws Exception {
+        // Context of the app1312 under test.
+        Context app1312Context = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("com.danielbispo.justjava", app1312Context.getPackageName());
+    }
+}
---
+++ app/src/main/java/com/danielbispo/justjava/MainActivity.java
@@ -0,0 +1,91 @@
+package com.danielbispo.justjava; /**
+ * Add your package below. Package name can be found in the project's AndroidManifest.xml file.
+ * This is the package name our example uses:
+ *
+ * package com.example.android.justjava; 
+ */
+
+import android.os.Bundle;
+import android.support.v7.app1312.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.danielbispo.justjava.R;
+
+import java.text.NumberFormat;
+
+/**
+ * This app1312 displays an order form to order coffee.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    static int numberOfCoffees;
+    Button btn1;
+    Button btnPlus;
+    Button btnMinus;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn1 = (Button) findViewById(R.id.orderBtn);
+        btnPlus = (Button) findViewById(R.id.plusBtn);
+        btnMinus = (Button) findViewById(R.id.minusBtn);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                submitOrder(getCurrentFocus(), numberOfCoffees);
+            }
+        });
+
+        btnMinus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                lessCount(getCurrentFocus());
+            }
+        });
+
+        btnPlus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                plusCount(getCurrentFocus());
+            }
+        });
+    }
+
+    /**
+     * This method is called when the order button is clicked.
+     */
+    public void submitOrder(View view, int numberOfCoffees) {
+        display(numberOfCoffees);
+        displayPrice(numberOfCoffees * 2);
+    }
+
+    /**
+     * This method displays the given quantity value on the screen.
+     */
+    private void display(int number) {
+        TextView quantityTextView = (TextView) findViewById(R.id.itemCount);
+        quantityTextView.setText("" + number);
+    }
+    /**
+     * This method displays the given price on the screen.
+     */
+    private void displayPrice(int number) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
+    private void plusCount(View view) {
+        TextView count = (TextView) findViewById(R.id.itemCount);
+        numberOfCoffees = ++numberOfCoffees;
+        count.setText(""+numberOfCoffees);
+    }
+    private void lessCount(View view) {
+        TextView priceTextView = (TextView) findViewById(R.id.itemCount);
+        numberOfCoffees = --numberOfCoffees;
+        priceTextView.setText(""+numberOfCoffees);
+    }
+}
